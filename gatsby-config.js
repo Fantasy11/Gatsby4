7b3abad3291b2cb61d1298ef5527218fdf5f0802
zcf <605694021@qq.com>
2021-06-25 20:08:57 +0800

    sec commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+ module.exports = {
+  siteMetadata: {
+    title: `Pandas eating`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
